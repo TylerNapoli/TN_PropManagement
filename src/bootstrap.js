8097d159a4b60dd2ee19d1298b75c397caca0585
Code <Code@batc.local>
2018-07-31 09:48:27 -0600

    added redux-form

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
-//import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
 import Layout from './components/layout';
 
 import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
-
 
 function main() {
   ReactDOM.render(
