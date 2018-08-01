5328cb5125f57ef6cc88e3d34ab0d7cd3a74f163
Code <Code@batc.local>
2018-08-01 11:36:47 -0600

    installed redux-thunk and axios. built auth actions

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import SignupForm from './signupForm';
 
 class Signup extends Component {
+
+    onSubmit = (fields) => {
+
+    }
+
     render() {
         return (
-            <div className="sign-up">
-                <SignupForm />
+            <div className='sign-up'>
+                <SignupForm onSubmit={(event) => this.onSubmit(event)}/>
             </div>
-        );
+        )
     }
 }
 
